0b8b8b15018e7d534c067564cc026fe41b0af463
argosputnik <schmidtjai@yandex.com>
2025-05-02 11:09:52 +0000

    .

--- cars/asgi.py
+++ cars/asgi.py
@@ -1,11 +1,14 @@
-# asgi.py
 import os
+# Set the settings module before any Django imports
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars.settings')
+
+# Now import Django and Channels components
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import cars.routing  # Import your routing module
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars.settings')
+# Import your routing module after Django settings are configured
+import cars.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
