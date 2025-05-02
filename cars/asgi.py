e9e20a015874cf4ee102c6b224820513f64ef6c1
argosputnik <schmidtjai@yandex.com>
2025-05-02 10:54:11 +0000

    .

--- cars/asgi.py
+++ cars/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import cars.routing  # Import your routing module
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
