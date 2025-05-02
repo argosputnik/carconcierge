4cbe48a18280a22d2ca20ef75e354e04eddd304c
argosputnik <schmidtjai@yandex.com>
2025-05-02 10:43:50 +0000

    .

--- cars/consumers.py
+++ cars/consumers.py
@@ -4,7 +4,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from .models import ServiceRequest
+from main.models import ServiceRequest
 
 User = get_user_model()
 
