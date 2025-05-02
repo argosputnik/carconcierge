57f9730281c57484462062a05433647a72ef546b
argosputnik <schmidtjai@yandex.com>
2025-05-02 10:36:01 +0000

    .

--- cars/consumers.py
+++ cars/consumers.py
@@ -1,4 +1,5 @@
 # consumers.py
+
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
@@ -11,15 +12,13 @@ class LocationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.request_id = self.scope['url_route']['kwargs']['request_id']
         self.group_name = f"location_{self.request_id}"
-        
-        # Get the user from the scope
-        user = self.scope["user"]
-        
-        # Check if user has permission to access this service request
-        if not await self.has_permission(user, self.request_id):
+        self.user = self.scope["user"]
+
+        # Permission check: only allow customer, assigned_to, or staff roles
+        if not await self.has_permission(self.user, self.request_id):
             await self.close()
             return
-            
+
         # Join group
         await self.channel_layer.group_add(
             self.group_name,
@@ -33,42 +32,50 @@ class LocationConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    # Receive message from WebSocket (from concierge)
     async def receive(self, text_data):
+        """
+        Receives a message from the WebSocket.
+        If the user is a concierge, update the DB and broadcast to group.
+        """
         data = json.loads(text_data)
-        
-        # Save location to database if sender is concierge
-        user = self.scope["user"]
-        if user.role == 'concierge':
-            await self.update_location(self.request_id, data['lat'], data['lng'])
-        
-        # Broadcast to group
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                'type': 'location_update',
-                'lat': data['lat'],
-                'lng': data['lng'],
-            }
-        )
+        lat = data.get('lat')
+        lng = data.get('lng')
 
-    # Receive message from group
+        # Only concierge can send location updates
+        if self.user.role == 'concierge':
+            await self.update_location(self.request_id, lat, lng)
+
+            # Broadcast to group
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    'type': 'location_update',
+                    'lat': lat,
+                    'lng': lng,
+                }
+            )
+
     async def location_update(self, event):
+        """
+        Called when a location update is sent to the group.
+        """
         await self.send(text_data=json.dumps({
             'lat': event['lat'],
             'lng': event['lng'],
         }))
-    
+
     @database_sync_to_async
     def has_permission(self, user, request_id):
         try:
             sr = ServiceRequest.objects.get(id=request_id)
-            return (user == sr.customer or 
-                    user == sr.assigned_to or 
-                    user.role in ['concierge', 'dealer', 'owner'])
+            return (
+                user == sr.customer or
+                user == sr.assigned_to or
+                user.role in ['concierge', 'dealer', 'owner']
+            )
         except ServiceRequest.DoesNotExist:
             return False
-    
+
     @database_sync_to_async
     def update_location(self, request_id, lat, lng):
         try:
@@ -76,6 +83,5 @@ class LocationConsumer(AsyncWebsocketConsumer):
             sr.concierge_latitude = lat
             sr.concierge_longitude = lng
             sr.save(update_fields=['concierge_latitude', 'concierge_longitude'])
-            return True
         except ServiceRequest.DoesNotExist:
-            return False
+            pass
