6e1c1be5ae20245508081245ddb83e0eb54c69a1
argosputnik <schmidtjai@yandex.com>
2025-05-04 01:26:24 +0000

    .

--- main/forms.py
+++ main/forms.py
@@ -133,7 +133,7 @@ class EditRequestForm(BootstrapFormMixin, forms.ModelForm):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
-{% comment %}
+"""
 # Determine who can edit sensitive fields
         can_edit_sensitive_fields = user and user.role in ['concierge', 'owner']
         # Determine who can edit location fields (adjust logic as needed)
@@ -184,7 +184,7 @@ class EditRequestForm(BootstrapFormMixin, forms.ModelForm):
         if 'assigned_to' in self.fields:
              self.fields['assigned_to'].required = False
 
-{% endcomment %}
+"""
 
 # --------------------------
 # Car Forms
