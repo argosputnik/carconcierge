83a998d2f33296641bc62968a404cfec6ba985fe
argosputnik <schmidtjai@yandex.com>
2025-05-05 13:17:32 +0000

    .

--- cars/routing.py
+++ cars/routing.py
@@ -4,6 +4,6 @@ from cars.consumers import LocationConsumer
 
 # Use a late-binding approach
 websocket_urlpatterns = [
-   re_path(r'^ws/location/(?P<service_request_id>\d+)/$', LocationConsumer.as_asgi()),
+   re_path(r'^ws/location/(?P<request_id>\d+)/$', LocationConsumer.as_asgi()),
 ]
 
