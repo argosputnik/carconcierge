652da0525edfa0c275ad418f5e72f8fa1fc99278
argosputnik <schmidtjai@yandex.com>
2025-05-05 13:04:22 +0000

    .

--- cars/routing.py
+++ cars/routing.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
-from cars.consumers import MyConsumer
+from cars.consumers import LocationConsumer
 
 
 # Use a late-binding approach
 websocket_urlpatterns = [
-   re_path(r'^ws/location/(?P<service_request_id>\d+)/$', MyConsumer.as_asgi()),
+   re_path(r'^ws/location/(?P<service_request_id>\d+)/$', LocationConsumer.as_asgi()),
 ]
 
